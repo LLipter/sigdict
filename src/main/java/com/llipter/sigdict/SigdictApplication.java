78db5b445ffcbf99a30276399dc00e2a2e37ade8
LLipter <katsudonn@163.com>
2019-12-17 05:59:36 -0800

    init

---
+++ src/main/java/com/llipter/sigdict/SigdictApplication.java
@@ -0,0 +1,13 @@
+package com.llipter.sigdict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigdictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigdictApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/llipter/sigdict/SigdictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llipter.sigdict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigdictApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
