2f648b11f97e7c8532bf8c34eb981faab64c330d
LLipter <katsudonn@163.com>
2019-12-18 03:47:14 -0800

    password validating enabled

--- src/main/java/com/llipter/sigdict/repository/UserRepository.java
+++ src/main/java/com/llipter/sigdict/repository/UserRepository.java
@@ -6,5 +6,5 @@ import com.llipter.sigdict.entity.User;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 public interface UserRepository extends CrudRepository<User, Integer> {
-
+    User findByUsername(String username);
 }
