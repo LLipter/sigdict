ddf01a413b05de197762c2abb72ebffff3843aee
LLipter <katsudonn@163.com>
2019-12-19 03:51:00 -0800

    main page session control enabled

--- src/main/java/com/llipter/sigdict/controller/MainController.java
+++ src/main/java/com/llipter/sigdict/controller/MainController.java
@@ -1,14 +1,22 @@
 package com.llipter.sigdict.controller;
 
+import com.llipter.sigdict.Utility;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
-public class MainController {
+public class MainController extends SessionController {
     @RequestMapping(value = "/main.html", method = RequestMethod.GET)
-    public String loginPage(Model model) {
+    public String loginPage(Model model, HttpServletRequest request) {
+        if (!validateSession(request)) {
+            // haven't signed in
+            Utility.addErrorMessage(model, "PLEASE SIGN IN FIRST");
+            return "login";
+        }
         return "main";
     }
 }
